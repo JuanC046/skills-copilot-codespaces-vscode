0354906ec9c3dc0184c4b6abdd102267e3979706
JuanC046 <128242516+JuanC046@users.noreply.github.com>
2023-11-12 21:36:37 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember()
+{
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var project = document.getElementById("project");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    member.style.display = "block";
+    skills.style.display = "none";
+    project.style.display = "none";
+    contact.style.display = "none";
+    about.style.display = "none";
+}
+
